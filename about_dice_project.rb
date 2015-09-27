6cb01072a8e09b3ca4c08f8b4adad5d1efd6d899
Austin Smith <asmith17@madisoncollege.edu>
2015-09-27 18:20:24 +0000

    about_inheritance complete

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -1,10 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+
+class DiceSet
+  attr_reader :values
+  
+# Takes the number of dice and then maps a random number between 1 and 6 to
+# each of the slots
+  def roll(number_of_dice)
+    @values = (1..number_of_dice).map{rand(6) + 1}
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
